e25dbabedce3f81680be85d52f7294633a597fce
arshreality <arshdeep74644@gmail.com>
2019-08-15 16:49:03 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,39 @@
+from keep_alive import keep_alive
+import discord
+
+TOKEN = 'NjA5OTgyMTQyMDkyNDEwODgw.XU-pwA.-jeMAdqEiWLHDnEnJb0DiGgXZRo'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    num = 0
+    if message.content.startswith('!soccer'):
+        msg = message.content.replace('!soccer','')
+        my_list = msg.split(";")
+        for i in range(len(my_list)):
+            num = my_list[i].count(',')
+            if(num == 0):
+                my_list[i] = ".\t\t\t\t\t\t\t" + my_list[i]
+            if(num == 1):
+                my_list[i] = my_list[i].replace(",", "\t\t\t\t")
+                my_list[i] = ".\t\t\t" + my_list[i]
+            if(num == 2):
+                my_list[i] = my_list[i].replace(",", "\t\t")
+                my_list[i] = ".\t\t" + my_list[i]
+            if(num == 3):
+                my_list[i] = my_list[i].replace(",", "\t")
+                my_list[i] = ".\t" + my_list[i]
+        print(my_list)
+        [await client.send_message(message.channel, '\n' + (my_list[i])) for i in range(len(my_list))]
+
+keep_alive()
+client.run(TOKEN)
+
+
+# De Gea;Wan-Bissaka,Lindelof,Maguire,Dalot;Pogba,Pereira,McTominay;Mata;Rashford,Martial;
+# max(my_list, key=len)
